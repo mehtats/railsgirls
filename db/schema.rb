a7d9422f937ea54ec21af273eee91741ddd8a8b6
MAEHATA, Atsushi <atsushi.maehata@trictrac.co.jp>
2015-03-26 21:03:54 +0900

    userにageを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325105257) do
+ActiveRecord::Schema.define(version: 20150326113019) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "user_name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150325105257) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
